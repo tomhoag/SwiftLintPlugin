aab1c7aeccd247f36ca289521ea54e938d2af4e2
Tom Hoag <125505511+tom-tomesoftware@users.noreply.github.com>
2025-03-30 12:49:24 -0400

    moving stuff around

--- Package.swift
+++ Package.swift
@@ -17,7 +17,7 @@ let package = Package(
             name: "SwiftLintPlugin",
             capability: .buildTool(),
             dependencies: [],
-            path: "Plugins"
-        ),
+            path: "Sources"
+        )
     ]
 )
